8502cbda9ab108ba049a77cf9976e490dbd5dd36
Maria Bontescu <maria.bontescu@yahoo.com>
2024-11-24 20:44:43 +0200

    Add project files.

---
+++ Bontescu_Maria_Lab7/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Bontescu_Maria_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Bontescu_Maria_Lab7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bontescu_Maria_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
