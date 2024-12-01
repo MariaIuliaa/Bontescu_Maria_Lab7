5d37d8117c6b7826df6364c6ad93f283de13814f
Maria Bontescu <maria.bontescu@yahoo.com>
2024-12-01 16:42:28 +0200

    Laborator 8

--- Bontescu_Maria_Lab7/App.xaml.cs
+++ Bontescu_Maria_Lab7/App.xaml.cs
@@ -1,15 +1,36 @@
-﻿namespace Bontescu_Maria_Lab7
+﻿using System;
+using System.IO;
+using Bontescu_Maria_Lab7.Data;
+
+namespace Bontescu_Maria_Lab7
 {
     public partial class App : Application
     {
+        // Singleton pentru baza de date
+        private static ShoppingListDatabase database;
+
+        // Proprietatea statică pentru accesarea bazei de date
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    // Creăm baza de date cu calea către directorul local al aplicației
+                    database = new ShoppingListDatabase(Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        // Constructorul aplicației
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            // Setăm pagina principală a aplicației
+            MainPage = new AppShell();
         }
     }
 }
