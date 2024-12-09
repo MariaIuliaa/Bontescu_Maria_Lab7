03fe51898d48cf3e23f9b3b223836ef03e0d595d
Maria Bontescu <maria.bontescu@yahoo.com>
2024-12-09 10:46:22 +0200

    Laborator9

--- Bontescu_Maria_Lab7/ListPage.xaml.cs
+++ Bontescu_Maria_Lab7/ListPage.xaml.cs
@@ -22,4 +22,43 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        if (listView.SelectedItem is Product selectedProduct)
+        {
+            bool isConfirmed = await DisplayAlert("Confirm Delete",
+                                                  $"Are you sure you want to delete {selectedProduct.Description}?",
+                                                  "Yes", "No");
+
+            if (isConfirmed)
+            {
+                await App.Database.DeleteProductAsync(selectedProduct);
+
+                listView.ItemsSource = await App.Database.GetProductsAsync();
+            }
+        }
+        else
+        {
+            await DisplayAlert("No Item Selected", "Please select an item to delete.", "OK");
+        }
+    }
+
+
 }
